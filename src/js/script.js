ae0d2b6f40b8a5566c938e387cbb1656b61667e3
Gary W <gary.wilpizeski@gmail.com>
2017-02-06 17:15:53 -0500

    Setting up files

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+console.log('Hello World!');
+
+var t1 = document.createElement('h1');
+var t2 = document.createElement('p');
+
+t1.className = 'siteHeader';
+t2.className = 'siteText';
+
+document.body.innerHTML = t1 + t2;
+
+t1.innerText = 'Hello World!';
+t2.innerHTML = 'Lorem ipsum dolor <span>sit amet</span>.'
