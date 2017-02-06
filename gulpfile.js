b920a55023f2e273e9835a0e2f164f912cd8a72b
Gary W <gary.wilpizeski@gmail.com>
2017-02-06 17:30:32 -0500

    setting up browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
+var browserSync = require('browser-sync').create();
 
 gulp.task('css', function() {
-    gulp.src('src/css/style.css')
+    gulp.src([
+        'src/css/normalize.css',
+        'src/css/style.css'
+    ])
+        .pipe(concatCss('styles.css'))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/resource/css'));
+        .pipe(gulp.dest('dist/resource/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['css'], function () {
+gulp.task('serve', function() {
+    browserSync.init({
+        server: "./"
+    });
+});
+
+gulp.task('default', ['serve','css'], function () {
     gulp.watch('src/css/style.css', ['css']);
+    gulp.watch('src/default.htm').on('change', browserSync.reload);
 });
