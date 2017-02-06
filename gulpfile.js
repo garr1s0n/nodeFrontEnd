4b2c422a9de8195b353844ac0614fb4f827a4421
Gary W <gary.wilpizeski@gmail.com>
2017-02-06 17:01:14 -0500

    setting up gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('css', function() {
     gulp.src('src/css/style.css')
+        .pipe(cleanCSS())
         .pipe(gulp.dest('dist/resource/css'));
+});
+
+gulp.task('default', ['css'], function () {
+    gulp.watch('src/css/style.css', ['css']);
 });
