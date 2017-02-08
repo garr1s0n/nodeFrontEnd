e75b82fd320142491fc196cfd148c8fdf666fed7
Gary W <gary.wilpizeski@gmail.com>
2017-02-08 15:51:49 -0500

    update base script

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,1 @@
 console.log('Hello World!');
-
-var t1 = document.createElement('h1');
-var t2 = document.createElement('p');
-
-t1.className = 'siteHeader';
-t2.className = 'siteText';
-
-document.body.innerHTML = t1 + t2;
-
-t1.innerText = 'Hello World!';
-t2.innerHTML = 'Lorem ipsum dolor <span>sit amet</span>.'
